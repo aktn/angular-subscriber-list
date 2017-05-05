e33218ba38faf7adeda8105f464a7041b7313d53
Aung Khant <theaungkhant@gmail.com>
2017-05-05 21:54:25 +0100

    Applying Form Validation

--- app/subscriber-app/components/subscriber-form/subscriber-form.component.ts
+++ app/subscriber-app/components/subscriber-form/subscriber-form.component.ts
@@ -4,11 +4,26 @@ import { Component } from '@angular/core';
     selector: 'subscriber-form',
     styleUrls: ['subscriber-form.component.scss'],
     template:`
-        <form>
-            <input type="text" name="name" placeholder="name" required>
-            <input type="text" name="email" placeholder="email" required>
+        <form #form="ngForm" novalidate>
+            <input type="text" 
+                   name="name" 
+                   placeholder="name" 
+                   #name="ngModel" 
+                   ngModel 
+                   required>
+            <div *ngIf="name.errors?.required && name.dirty">Name Required</div>
+
+            <input type="text" 
+                   name="email" 
+                   placeholder="email" 
+                   #email="ngModel" 
+                   ngModel 
+                   required >
+            <div *ngIf="email.errors?.required && email.dirty">Email Required</div>
+
             <button>Add</button>
             <button>Export</button>
+            {{ form.value | json }}
         </form>
     `
 })
